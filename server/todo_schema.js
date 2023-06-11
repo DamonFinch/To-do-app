e5b6436d19f728f840678b4fdb7092b945bda6c5
DamonFinch <132647647+DamonFinch@users.noreply.github.com>
2023-06-10 20:05:39 -0400

    Create todo_schema.js

---
+++ server/todo_schema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        required: true
+    },
+    onEditMode: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const todo = (module.exports = mongoose.model('todo', todoSchema))
